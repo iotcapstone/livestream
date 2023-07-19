f176fb2b23a6a1f64462bdc1c2ca8727571c6ae1
Capstone 2023 <capstone2023@iotlabserver.(none)>
2023-07-19 15:38:25 +1000

    added streamlit

---
+++ streamlit_app.py
@@ -0,0 +1,19 @@
+#importing streamlit library
+
+import streamlit as st
+
+
+
+#displaying a local video file
+
+video_file = open('FILENAME', 'rb') #enter the filename with filepath
+
+video_bytes = video_file.read() #reading the file
+
+st.video(video_bytes) #displaying the video
+
+
+
+#displaying a video by simply passing a Youtube link
+
+st.video("https://youtu.be/yVV_t_Tewvs")
